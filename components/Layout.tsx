85ec27584b1f09ee91f0a9f40fc2b27f52bc2c91
SrikantShubam <srikantshubham.personal@gmail.com>
2025-05-04 11:40:25 +0530

    footer more 4/n

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -30,7 +30,7 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [footerData, setFooterData] = useState<FooterData | null>(null);
+  const [footerData, setFooterData] = useState<FooterData | undefined>(undefined);
   const router = useRouter();
   const currentLocale = router.locale || 'en';
 
